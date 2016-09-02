6b8b3564ea2f9685699e3e0a299f2ca1b94c2f3f
jiez1812 <jieztee@gmail.com>
2016-09-02 22:58:20 +0800

    update arrayDS
    Solution of arrayDS

--- src/arrayDS.java
+++ src/arrayDS.java
@@ -5,36 +5,16 @@ import java.util.Scanner;
 
 public class arrayDS {
     public static void main(String[] args){
-        int[] arr, arr2;
-
-        System.out.print("Input array size: ");
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
+        int length = sc.nextInt();
+        int[] arr = new int[length];
 
-        arr = new int[i];
-        arr2 = new int[i];
-
-        Scanner arrIn = new Scanner(System.in);
-        for(int j = 0; j < arr.length; j++) {
-            System.out.print("Enter the array[" + (j + 1) + "] of Array(" + i + "):");
-            int num = arrIn.nextInt();
-            arr[j] = num;
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
         }
 
-
-        int temp, a = 0;
-
-        for(int b = arr.length - 1; b >= 0; b-- ) {
-            arr2[a] = arr[b];
-            a++;
-        }
-
-        for(int k = 0; k < arr.length;k++){
-            if (k <= arr.length) {
-                System.out.print(arr2[k] + ",");
-            }else{
-                System.out.print(arr2[k]);
-            }
+        for(int j = arr.length - 1; j >= 0; j--){
+            System.out.print(arr[j] + " ");
         }
     }
 }
